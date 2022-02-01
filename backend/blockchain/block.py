f60ae0d2f43d8a372ab8b068b3963c65522d74cc
Darania Muralitharan <Darania@Daranias-MacBook-Air.local>
2022-02-01 19:12:22 +0000

    Proof of Work System | Average Work Script

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,11 +1,15 @@
 import time
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
+
 
 GENESIS_DATA = {
     'timestamp': 1,
     'last_hash': 'genesis_last_hash',
     'hash': 'genesis_hash',
-    'data': []
+    'data': [],
+    'difficulty': 3,
+    'nonce': 'genesis_nonce'
 }
 
 class Block:
@@ -13,11 +17,13 @@ class Block:
     Block: a unit of storage.
     Store transactions in a blockchain that supports a cryptocurrency.
     """
-    def __init__(self, timestamp, last_hash, hash, data):
+    def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce):
         self.timestamp = timestamp
         self.last_hash = last_hash
         self.hash = hash
         self.data = data
+        self.difficulty = difficulty
+        self.nonce = nonce
 
     def __repr__(self):
         return (
@@ -25,19 +31,32 @@ class Block:
             f'timestamp: {self.timestamp}, '
             f'last_hash: {self.last_hash}, '
             f'hash: {self.hash}, '
-            f'hash: {self.hash})'
+            f'data: {self.data}, '
+            f'difficulty: {self.difficulty}, '
+            f'nonce: {self.nonce})'
         )
 
     @staticmethod
     def mine_block(last_block, data):
         """
-        Mine a block based on the given last_block and data. Calling it mining because training a block requires computational work and the analogy for spending CPU power is mining. 
+        Calling it mining because training a block requires computational work and the analogy for spending CPU power is mining. 
+        Mine a block based on the given last_block and data, until a block hash is found that meets the leading 0's proof of work requirement.
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        hash = crypto_hash(timestamp, last_hash, data)
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
+        nonce = 0
 
-        return Block(timestamp, last_hash, hash, data)
+        hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
+
+        while hash[0:difficulty] != '0' * difficulty:
+            nonce += 1
+            timestamp = time.time_ns() 
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
+            hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
+
+
+        return Block(timestamp, last_hash, hash, data, difficulty, nonce)
 
     @staticmethod
     def genesis():
@@ -50,8 +69,28 @@ class Block:
         #     hash = GENESIS_DATA['hash'],
         #     data = GENESIS_DATA['data']
         # )
+        # ** unpacking operatore used to unpack the data than write it out many times like above
 
         return Block(**GENESIS_DATA)
+
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+
+        """
+        Calculate the adjusted difficulty according to the MINE_RATE. 
+        Increase the difficulty for quickly mined blocks.
+        Decrease the difficulty for slowly mined blocks.
+        """
+
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+
+        if (last_block.difficulty - 1 ) > 0:
+         return last_block.difficulty - 1
+
+        return 1
+        
+
 
 
 def main():
